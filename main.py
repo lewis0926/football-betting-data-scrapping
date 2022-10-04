30891d88cd4567c01711ca3a6601fc4b54ce2eeb
Lewis Shum <lewishum96@gmail.com>
2022-10-04 16:38:20 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pymongo
+import configparser
+import logging
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+connStr = config.get("database", "connStr")
+
+logging.basicConfig(filename=config.get("logging", "filename"),
+                    encoding='utf-8',
+                    level=logging.INFO,
+                    format="%(asctime)s %(levelname)s: %(message)s")
+
+urls = ["https://bet.hkjc.com/football/odds/odds_had.aspx?lang=en&tournid=50000017"]
+
+
+def start():
+    client = pymongo.MongoClient(connStr, serverSelectionTimeoutMS=10000)
+    db = client["football-betting"]
+    collection = db["odds"]
+
+    try:
+        logging.info("Connected to DB server: " + client.server_info())
+    except Exception:
+        logging.error("Unable to connect to the server.")
+
+    try:
+        for url in urls:
+            logging.info("start scrapping data from url: " + url)
+
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver.maximize_window()
+            driver.get(url)
+            time.sleep(0.5)
+
+            dates = driver.find_elements(By.CLASS_NAME, "couponTable")
+            for date in dates:
+                events = date.text.split("\n")
+                competition = events[0]
+                odds = []
+                for i in range(1, len(events)):
+                    information = events[i].split(' ')
+                    sellingDate = datetime.datetime.strptime(information[0], "%d/%m/%Y").strftime("%Y-%m-%d")
+                    sellingTime = datetime.datetime.strptime(information[1], "%H:%M").strftime("%H:%M:%S")
+                    expectedStopSellingTime = datetime.datetime.strptime("{0}T{1}.000Z".format(sellingDate, sellingTime), "%Y-%m-%dT%H:%M:%S.000Z")
+                    teams = " ".join(information[3:-3]).split("vs")
+
+                    odds.append(
+                        {
+                            "competition": competition,
+                            "expectedStopSellingTime": expectedStopSellingTime,
+                            "eventId": information[2],
+                            "home": teams[0].strip(),
+                            "away": teams[1].strip(),
+                            "homeOdd": information[-3],
+                            "drawOdd": information[-2],
+                            "awayOdd": information[-1]
+                        }
+                    )
+
+                logging.info(odds)
+                collection.insert_many(odds)
+    except Exception as ex:
+        logging.error(ex)
+    finally:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    start()
+
